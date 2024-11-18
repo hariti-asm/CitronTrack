5154b7ebaeaa790283a69bddb2f522744a615439
Asmaa Hariti <haritiasmaa74@gmail.com>
2024-11-18 22:38:41 +0100

    adding sale response with validation

--- dto/sale/SaleResponseDTO.java
+++ dto/sale/SaleResponseDTO.java
@@ -1,13 +1,11 @@
 package ma.hariti.asmaa.wrm.citrontrack.dto.sale;
 
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
 import jakarta.validation.constraints.Positive;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.*;
 import ma.hariti.asmaa.wrm.citrontrack.dto.customer.CustomerDTO;
+import ma.hariti.asmaa.wrm.citrontrack.dto.harvest.HarvestDTO;
 
 import java.time.LocalDate;
 
@@ -15,20 +13,25 @@ import java.time.LocalDate;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class SaleRequestDTO {
+public class SaleResponseDTO {
 
-    @Positive(message = "Unit price must be positive")
+    private Long id;
+
+    @NotNull(message = "Unit price cannot be null")
+    @Positive(message = "Unit price must be greater than 0")
     private double unitPrice;
 
-    @Positive(message = "Quantity sold must be zero or positive")
+    @NotNull(message = "Quantity sold cannot be null")
+    @Positive(message = "Quantity sold must be greater than 0")
     private double quantitySold;
 
     @NotNull(message = "Date sold cannot be null")
     @PastOrPresent(message = "Date sold must be in the past or present")
     private LocalDate dateSold;
 
+    @NotNull(message = "Customer cannot be null")
     private CustomerDTO customer;
 
-    @NotNull(message = "Harvest ID cannot be null")
-    private Long harvestId;
+    @NotNull(message = "Harvest cannot be null")
+    private HarvestDTO harvest;
 }
