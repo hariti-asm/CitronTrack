eda9723b4c0fe09ea0dc3436fcb4d0aaa4f7eccb
Asmaa Hariti <haritiasmaa74@gmail.com>
2024-11-18 12:42:22 +0100

    putting project architectrure and making the global exceptions handler

---
+++ CitronTrackApplication.java
@@ -0,0 +1,13 @@
+package ma.hariti.asmaa.wrm.citrontrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitronTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitronTrackApplication.class, args);
+    }
+
+}
---
+++ util/BaseDTO.java
@@ -0,0 +1,9 @@
+package ma.hariti.asmaa.wrm.citrontrack.util;
+
+import lombok.Data;
+
+@Data
+public class BaseDTO {
+    private Long id;
+
+}
