6ba4d321e705881d86f4916251793676aa3330b4
Asmaa Hariti <haritiasmaa74@gmail.com>
2024-11-19 10:18:13 +0100

    Maps to the treeId field in HarvestDetailId

--- entity/HarvestDetail.java
+++ entity/HarvestDetail.java
@@ -6,7 +6,6 @@ import jakarta.validation.constraints.Positive;
 import lombok.*;
 import ma.hariti.asmaa.wrm.citrontrack.embeddedable.HarvestDetailId;
 import ma.hariti.asmaa.wrm.citrontrack.util.BaseEntity;
-
 @Entity
 @Table(name = "harvest_details")
 @Getter
@@ -19,15 +18,17 @@ public class HarvestDetail extends BaseEntity {
     private HarvestDetailId id;
 
     @NotNull(message = "Quantity cannot be null")
-    @Positive(message = "Quantity  harvested by tree must be greater than 0")
+    @Positive(message = "Quantity harvested by tree must be greater than 0")
     private Double quantity;
 
     @ManyToOne
+    @MapsId("treeId")
     @JoinColumn(name = "tree_id", nullable = false)
     @NotNull(message = "Tree must not be null")
     private Tree tree;
 
     @ManyToOne
+    @MapsId("harvestId")  // Maps to the harvestId field in HarvestDetailId
     @JoinColumn(name = "harvest_id", nullable = false)
     @NotNull(message = "Harvest must not be null")
     private Harvest harvest;
