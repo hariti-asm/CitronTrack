0d2186d681062b6c0794f16399312a6572d83773
Asmaa Hariti <haritiasmaa74@gmail.com>
2024-11-19 10:17:32 +0100

    fixing dupliocated foreign keys mapping

--- embeddedable/HarvestDetailId.java
+++ embeddedable/HarvestDetailId.java
@@ -1,6 +1,7 @@
 package ma.hariti.asmaa.wrm.citrontrack.embeddedable;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotNull;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -8,19 +9,17 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
-
 @Getter
 @Setter
 @NoArgsConstructor
 @EqualsAndHashCode
-
+@Embeddable
 public class HarvestDetailId implements Serializable {
     @NotNull(message = "Harvest ID cannot be null")
-    @Column(name = "harvet_id")
+    @Column(name = "harvest_id")
     private Long harvestId;
 
     @NotNull(message = "Tree ID cannot be null")
     @Column(name = "tree_id")
     private Long treeId;
-
 }
