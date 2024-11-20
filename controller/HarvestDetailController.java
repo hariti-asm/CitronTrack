4230a3c28bd693548bd06f91262c876ccdf4ab32
Asmaa Hariti <haritiasmaa74@gmail.com>
2024-11-20 23:05:16 +0100

    harvest detail controller

--- controller/HarvestDetailController.java
+++ controller/HarvestDetailController.java
@@ -1,4 +1,37 @@
 package ma.hariti.asmaa.wrm.citrontrack.controller;
 
+import jakarta.validation.Valid;
+import ma.hariti.asmaa.wrm.citrontrack.dto.harvestDetail.HarvestDetailRequestDTO;
+import ma.hariti.asmaa.wrm.citrontrack.dto.harvestDetail.HarvestDetailResponseDTO;
+import ma.hariti.asmaa.wrm.citrontrack.embeddedable.HarvestDetailId;
+import ma.hariti.asmaa.wrm.citrontrack.service.harvestDetail.HarvestDetailService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/harvest-details")
 public class HarvestDetailController {
+
+    private final HarvestDetailService harvestDetailService;
+
+    public HarvestDetailController(HarvestDetailService harvestDetailService) {
+        this.harvestDetailService = harvestDetailService;
+    }
+
+    @PostMapping
+    public ResponseEntity<HarvestDetailResponseDTO> createHarvestDetail(
+            @Valid @RequestBody HarvestDetailRequestDTO requestDTO) {
+        HarvestDetailResponseDTO createdHarvestDetail = harvestDetailService.createFromRequest(requestDTO);
+        return ResponseEntity.ok(createdHarvestDetail);
+    }
+
+    @PutMapping("/{harvestId}/{treeId}")
+    public ResponseEntity<HarvestDetailResponseDTO> updateHarvestDetail(
+            @PathVariable Long harvestId,
+            @PathVariable Long treeId,
+            @Valid @RequestBody HarvestDetailRequestDTO requestDTO) {
+        HarvestDetailId id = new HarvestDetailId(harvestId, treeId);
+        HarvestDetailResponseDTO updatedHarvestDetail = harvestDetailService.updateFromRequest(id, requestDTO);
+        return ResponseEntity.ok(updatedHarvestDetail);
+    }
 }
--- embeddedable/HarvestDetailId.java
+++ embeddedable/HarvestDetailId.java
@@ -3,16 +3,15 @@ package ma.hariti.asmaa.wrm.citrontrack.embeddedable;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
 import jakarta.validation.constraints.NotNull;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import java.io.Serializable;
 @Getter
 @Setter
 @NoArgsConstructor
 @EqualsAndHashCode
+@AllArgsConstructor
+
 @Embeddable
 public class HarvestDetailId implements Serializable {
     @NotNull(message = "Harvest ID cannot be null")
@@ -22,4 +21,6 @@ public class HarvestDetailId implements Serializable {
     @NotNull(message = "Tree ID cannot be null")
     @Column(name = "tree_id")
     private Long treeId;
+
+
 }
